7845a9fb4b8a7b3f654f8e9848913301332d988a
蛯名麗斗 <ebinareito@MacBook-Pro-od-ebinareito.local>
2020-11-19 12:05:10 +0900

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rate_apps.wsgi --log-file -
---
+++ rate_apps/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+import dj_database_url
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
